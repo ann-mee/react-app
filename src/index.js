1aea1a33a9c473063106ca9953fbc66496305212
Anna <anna.morawska@frontkom.com>
2023-09-07 13:48:05 +0200

    React lazy

--- src/index.js
+++ src/index.js
@@ -4,27 +4,73 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Refs from "./pages/Refs";
-import Hoc from "./pages/Hoc";
-import ControlledComponents from "./pages/ControlledComponents";
-import PureComponent from "./pages/PureComponent";
-import Optimization from "./pages/Optimization";
+const Home = React.lazy(() => import("./pages/Home"));
+const Refs = React.lazy(() => import("./pages/Refs"));
+const Hoc = React.lazy(() => import("./pages/Hoc"));
+const ControlledComponents = React.lazy(() =>
+  import("./pages/ControlledComponents")
+);
+const PureComponent = React.lazy(() => import("./pages/PureComponent"));
+const Optimization = React.lazy(() => import("./pages/Optimization"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="refs" element={<Refs />} />
-          <Route path="hoc" element={<Hoc />} />
+          <Route
+            index
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Home />
+              </React.Suspense>
+            }
+          />
+
+          <Route
+            path="refs"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Refs />
+              </React.Suspense>
+            }
+          />
+
+          <Route
+            path="hoc"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Hoc />
+              </React.Suspense>
+            }
+          />
+
           <Route
             path="controlled-components"
-            element={<ControlledComponents />}
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <ControlledComponents />
+              </React.Suspense>
+            }
           />
-          <Route path="pure-component" element={<PureComponent />} />
-          <Route path="optimization" element={<Optimization />} />
+
+          <Route
+            path="pure-component"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <PureComponent />
+              </React.Suspense>
+            }
+          />
+
+          <Route
+            path="optimization"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Optimization />
+              </React.Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
